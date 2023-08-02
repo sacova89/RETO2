f6330d91b4b262955f5830d125e30e099e0e3723
Santiago Acosta <sacova89@gmail.com>
2023-08-01 21:23:47 -0500

    se resolvieron bugs
    solución debug Pause controller

--- Assets/OArielG/Scripts/PauseController.cs
+++ Assets/OArielG/Scripts/PauseController.cs
@@ -10,24 +10,22 @@ public class PauseController : MonoBehaviour
                                             // este script se haya asociado a los botones.
     
     public void Pause(){
-        Debug.Log("BUTTON -> Pause");
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
     }
 
     public void Resume(){
-        Debug.Log("BUTTON -> Resume");
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
     }
 
     public void goMenu(int _escena){
-        Debug.Log("BUTTON -> Home");
+       
         Time.timeScale = 1f;
         SceneManager.LoadScene(_escena);
     }
 
     public void Audio(){
-        Debug.Log("BUTTON -> Audio");
+  
     }
 }
